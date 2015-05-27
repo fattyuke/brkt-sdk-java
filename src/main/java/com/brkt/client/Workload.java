5c639df2685aaee7d0d92de60e6bcd3887496e66
Boris Burtin <boris@brkt.com>
2015-05-27 15:38:07 -0700

    Make Workload member variables private
    Not sure why I made these public in the first place.

--- src/main/java/com/brkt/client/Workload.java
+++ src/main/java/com/brkt/client/Workload.java
@@ -19,18 +19,18 @@ public class Workload extends BrktResource {
     public static final String METADATA = "metadata";
     public static final String REQUESTED_STATE = "requested_state";
 
-    public String billingGroup;
-    public Boolean deleted;
-    public Boolean enableServiceDomain;
-    public Boolean expired;
-    public Timestamp leaseExpireTime;
-    public Timestamp leaseModifiedTime;
-    public BigDecimal maxCost;
-    public Map<String, String> metadata;
-    public Constants.RequestedState requestedState;
-    public String serviceDomain;
-    public String workloadTemplate;
-    public String zone;
+    private String billingGroup;
+    private Boolean deleted;
+    private Boolean enableServiceDomain;
+    private Boolean expired;
+    private Timestamp leaseExpireTime;
+    private Timestamp leaseModifiedTime;
+    private BigDecimal maxCost;
+    private Map<String, String> metadata;
+    private Constants.RequestedState requestedState;
+    private String serviceDomain;
+    private String workloadTemplate;
+    private String zone;
 
     public String getBillingGroupId() {
         return billingGroup;
